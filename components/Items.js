9c5f9197c8c99fe1a6e82f9df976fe47d3273f25
OrayKT <oray.kt@gmail.com>
2019-03-21 01:26:31 +0100

    return data.items.map

---
+++ components/Items.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import styled from 'styled-components';
+import gql from 'graphql-tag';
+
+const ALL_ITEMS_QUERY = gql`
+    query ALL_ITEMS_QUERY {
+        items{
+            id
+            title
+            price
+            description
+            image
+            largeImage
+        }
+    }
+`;
+
+const Center = styled.div`
+    text-align:center;
+`;
+
+const ItemsList = styled.div`
+    display: grid;
+    grid-auto-columns: 1fr 1fr;
+    grid-gap: 60px;
+    max-width: ${props => props.theme.maxWidth};
+    margin: 0 auto;
+`;
+
+class Items extends Component {
+    render() {
+        return (
+            <Center>
+                <Query query={ALL_ITEMS_QUERY}>
+                    {({ data, error, loading }) => {
+                        if (loading) return <p>Loading...</p>
+                        if (error) return <p>Error : {error.message}</p>
+                        return <p>{data.items.length}</p>
+                    }}
+                </Query>
+            </Center>
+        );
+    }
+}
+
+export default Items;
